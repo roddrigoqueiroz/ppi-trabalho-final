a0cda497904e130d69599bbed4bfb3c99d187197
Rodrigo <rodrigo.queiroz.neves@gmail.com>
2024-09-29 22:44:24 -0300

    Fix: Corrigindo últiams alterações antes de subir arquivo

--- front-end/js/meus-anuncios.js
+++ front-end/js/meus-anuncios.js
@@ -3,7 +3,6 @@ document.querySelectorAll('.btn-delete').forEach(button => {
     button.addEventListener('click', function() {
         if (confirm('Tem certeza que deseja excluir este anúncio?')) {
             alert('Anúncio excluído com sucesso!');
-            // Aqui você pode adicionar lógica de exclusão real quando estiver conectado ao backend
         }
     });
 });
